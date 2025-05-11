ef68c2d36ea79f3926ba4321ae979a4f79ba7e96
Abdallah Zahran <106187621+abdallahzahran@users.noreply.github.com>
2025-05-11 21:12:21 +0300

    Update shared_functions.py

--- api/routes/shared_functions.py
+++ api/routes/shared_functions.py
@@ -10,9 +10,9 @@ import cloudinary.api
 
 # Cloudinary configuration
 cloudinary.config(
-    cloud_name='dot3oekpp',
-    api_key='957184771196124',
-    api_secret='y6Gzc0n8iy0KGKQztqwrtcYQ_E4',
+    cloud_name='daa9je4uh',
+    api_key='828536938959528',
+    api_secret='7PqmY4ZEmrnxmmj6-mhOSeQFlOM',
     secure=True
 )
 
